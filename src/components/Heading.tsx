3b058a18d695b212ad8abf80ccb850b27ab2d57b
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-02-24 16:47:59 +0700

    update meta info

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading: React.FC = () => (
     <>
-        <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-br from-pink-500 to-rose-500 font-extrabold text-3xl md:text-4xl lg:text-6xl">
+        <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-tl from-pink-500 to-rose-500 font-extrabold text-3xl md:text-4xl lg:text-6xl">
             SVG Reader
             </h1>
         <h2 className="font-medium text-lg text-center text-gray-500 mt-2 w-4/5 sm:w-auto mx-auto">Read and preview your SVG file here!</h2>
