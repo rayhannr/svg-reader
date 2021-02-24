6cc821dcdfe7071825961e01bf0f0cbce52f8e96
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-02-24 23:00:21 +0700

    fix logic to convert style

--- src/util/App.ts
+++ src/util/App.ts
@@ -7,7 +7,7 @@ export const filterHtml = (html: string) => {
     return html
 }
 
-export const convertAttributeToCamelCase = (str: string, boundary: string) => {
+const convertAttributeToCamelCase = (str: string, boundary: string) => {
     return str.split(boundary).map((item, index) => index ? item.charAt(0).toUpperCase() + item.slice(1).toLowerCase() : item.toLowerCase()).join("")
 }
 
@@ -15,38 +15,38 @@ interface styleValues {
     [prop: string]: string
 }
 
-const getStyleObjectFromString = (str: string[]) => {
-    const style: styleValues = {}
+const STYLE_REGEX: RegExp = /(style\s*=\s*)("|')([^"]*?)(\s*)("|')/gm
 
-    const styleValue = str[3]
+const getStyleObjectFromString = (str: string) => {
+    const style: styleValues = {}
+    const executedStr = STYLE_REGEX.exec(str)!
+    STYLE_REGEX.lastIndex = 0
+
+    const styleValue = executedStr[3]
     styleValue.split(";").forEach(el => {
-      const [property, value] = el.split(":")
-      if (!property) return
-  
-      const formattedProperty = convertAttributeToCamelCase(property.trim(), '-')
-      style[formattedProperty] = value.trim()
+        const [property, value] = el.split(":")
+        if (!property) return
+
+        const formattedProperty = convertAttributeToCamelCase(property.trim(), '-')
+        style[formattedProperty] = value.trim()
     })
-    
+
     let outputStyle = 'style={{'
-    for(const property in style){
+    for (const property in style) {
         outputStyle += ` ${property}: "${style[property]}",`
     }
-    
+
     outputStyle = outputStyle.slice(0, -1) //remove last comma in the string
-    outputStyle += " }}"
-  
+    outputStyle += " }}" //and replace it with this instead
+
     return outputStyle
-  }
+}
 
 export const convertToJsx = (html: string) => {
     html = filterHtml(html)
     html = html.replace(/\s(\w+)-(\w)([\w-]*)=/gm, str => convertAttributeToCamelCase(str, '-'))
     html = html.replace(/\s(\w+):(\w)([\w-]*)=/gm, str => convertAttributeToCamelCase(str, ':'))
-
-    let styleAttribute: string[] | null = /(style\s*=\s*)("|')([^"]*?)(\s*)("|')/gm.exec(html)
-    if(styleAttribute){
-        html = html.replace(styleAttribute[0], getStyleObjectFromString(styleAttribute))
-    }
+    html = html.replace(STYLE_REGEX, str => getStyleObjectFromString(str))
 
     html = html.replace(/("|'\s*)(\d+)(px)*(\s*"|')/gm, "{$2}")
     return html
